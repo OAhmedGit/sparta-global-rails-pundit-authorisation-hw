302801150a9f433fc7a9b68f91ae30f6632b1445
Osama Ahmed <oahmed@spartaglobal.com>
2018-05-17 22:34:50 +0100

    removed user_controller as it was not required

--- app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < ApplicationController
-  def update
-    @user = User.find(params[:id])
-    @user.toggle!(:admin)
-    flash[:success] = 'OK!'
-    redirect_to root_path
-  end
-end
